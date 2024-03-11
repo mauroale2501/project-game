8dcfb05c31206f1c117d238e4362c2d65fd47829
Mauro Morales <mauroale2501@gmail.com>
2024-03-11 10:52:34 +0100

    Implement Hint.tsx

--- frontend/src/components/Hint.tsx
+++ frontend/src/components/Hint.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
 const Hint = () => {
-  return <div></div>;
+  const [hint, setHint] = useState("");
+
+  const fetchHint = async () => {
+    try {
+      const response = await fetch("/api/hints");
+      if (!response.ok) {
+        throw new Error("Failed fetch");
+      }
+      const data = await response.json();
+      setHint(data.hint);
+    } catch (error) {
+      console.error("Error fetching hint:", error);
+    }
+  }; // need to implement cut time in half
+
+  return (
+    <div>
+      <Button onClick={fetchHint}>Get Hint</Button>
+      {hint && <p>{hint}</p>}
+    </div>
+  );
 };
 
 export default Hint;
