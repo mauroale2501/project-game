b6596c7ac643d694964b692360bc5c2b334f9b5b
Mauro Morales <mauroale2501@gmail.com>
2024-03-11 12:13:01 +0100

    WIP: Fetch button keyLevel2

--- frontend/src/components/Level2.tsx
+++ frontend/src/components/Level2.tsx
@@ -3,12 +3,27 @@ import Hint from "./Hint";
 import Input from "./Input";
 import Timer from "./Timer";
 import "../styles/Level2.css";
+import { useState } from "react";
 
 type Level2Props = {
   initialTime: number;
 };
 
 const Level2 = ({ initialTime }: Level2Props) => {
+  const [keyLevel2, setKeyLevel2] = useState();
+
+  const fetchLevel2 = async () => {
+    try {
+      const response = await fetch("/api/keyLevel2");
+      if (!response.ok) {
+        throw new Error("Failed fetch");
+      }
+      const data = await response.json();
+      setKeyLevel2(data.message);
+    } catch (error) {
+      console.error("Error fetching hint:", error);
+    }
+  };
   return (
     <div className="all-level2">
       <div className="time-pass-level">
@@ -16,7 +31,8 @@ const Level2 = ({ initialTime }: Level2Props) => {
           <Timer initialTime={initialTime} />
         </div>
         <div className="button-level2">
-          <Button>Pass to next level</Button>
+          <Button onClick={fetchLevel2}>Pass to next level</Button>
+          {keyLevel2 && <p>{keyLevel2}</p>}
         </div>
       </div>
       <section className="hint-level2">
