1e97b480b2bdb4533eece197edc4282ebc9fbcbd
Mauro Morales <mauroale2501@gmail.com>
2024-03-11 11:33:38 +0100

    WIP: Implementing Level2

--- frontend/src/components/Level2.tsx
+++ frontend/src/components/Level2.tsx
@@ -1,5 +1,37 @@
-const Level2 = () => {
-  return <div className="all-level2"></div>;
+import { Button } from "react-bootstrap";
+import Hint from "./Hint";
+import Input from "./Input";
+import Timer from "./Timer";
+import "../styles/Level2.css";
+
+type Level2Props = {
+  initialTime: number;
+};
+
+const Level2 = ({ initialTime }: Level2Props) => {
+  return (
+    <div className="all-level2">
+      <div className="time-pass-level">
+        <div className="timer">
+          <Timer initialTime={initialTime} />
+        </div>
+        <div className="button-level2">
+          <Button>Pass to next level</Button>
+        </div>
+      </div>
+      <section className="hint-level2">
+        <h2>Rules of Hint:</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. autem dicta
+          quod minus porro.
+        </p>
+        <Hint />
+      </section>
+      <div className="input-level2">
+        <Input currentLevel={2} />
+      </div>
+    </div>
+  );
 };
 
 export default Level2;
